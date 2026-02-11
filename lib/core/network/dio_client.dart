07757ab7395d3fb081637a9c2382a24af085cdce
Idris Ali Bello <idrisalibello@gmail.com>
2026-02-11 07:19:44 +0100

    update 5: SeamlessCall Flutter app

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -19,7 +19,7 @@ class DioClient {
   DioClient._internal() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://192.168.1.221/seamless_call/',
+        baseUrl: 'http://10.207.100.51/seamless_call/',
         connectTimeout: const Duration(seconds: 60),
         receiveTimeout: const Duration(seconds: 15),
         headers: {
